41188eb5ac0c91e230236e0b3986a06947b797e5
PandaNoir <moon_in_france@yahoo.co.jp>
2015-11-27 16:23:47 +0900

    install gulp-tsc

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var ts = require('gulp-tsc');
 
 gulp.task('default', function() {
     return browserify({
         entries: ['./src/browser.js']
     }).bundle().pipe(source('unitary.browser.js')).pipe(gulp.dest('./dist'));
+});
+gulp.task('compile', function() {
+    return gulp.src('src/unitary.ts')
+        .pipe(ts())
+        .pipe(gulp.dest('dist'));
 });
