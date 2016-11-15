fd078f1abd7cee4228bfa69d2fb7ce77e75226aa
幾野直人 <moon_in_france@yahoo.co.jp>
2016-11-15 09:14:40 +0900

    clear up gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,33 @@ const rename = require("gulp-rename");
 const gzip = require('gulp-gzip');
 
 gulp.task('minify', () => {
-    gulp.src('./dist/canvas.js')
-    .pipe(closureCompiler({
-        // compilerPath: './bower_components/closure-compiler/compiler.jar',
-        fileName: 'canvas.min.js',
-        compilerFlags: {
+    const flags = {
             compilation_level: 'SIMPLE_OPTIMIZATIONS',
             language_in: 'ECMASCRIPT5_STRICT',
             warning_level: 'QUIET'
-        }
+        };
+    gulp.src('./dist/canvas.js')
+    .pipe(closureCompiler({
+        fileName: 'canvas.min.js',
+        compilerFlags: flags
     }))
     .pipe(gulp.dest('./dist/'))
     .pipe(gzip()).pipe(gulp.dest('./dist/'));
 
     gulp.src('./dist/unitary.js')
     .pipe(closureCompiler({
-        // compilerPath: './bower_components/closure-compiler/compiler.jar',
         fileName: 'unitary.min.js',
-        compilerFlags: {
-            compilation_level: 'SIMPLE_OPTIMIZATIONS',
-            language_in: 'ECMASCRIPT5_STRICT',
-            warning_level: 'QUIET'
-        }
+        compilerFlags: flags
     }))
     .pipe(gulp.dest('./dist/'))
     .pipe(gzip()).pipe(gulp.dest('./dist/'))
     .on('end', () => {
-        gulp.src('./dist/unitary.js')
-            .pipe(rename('unitary.browser.js'))
+        const files = ['unitary.browser.js', 'unitary.browser.min.js', 'unitary.browser.min.js.gz'];
+        for (const file of files) {
+        gulp.src('./dist/' + file.replace('.browser', ''))
+            .pipe(rename(file))
             .pipe(gulp.dest('./dist'));
-
-        gulp.src('./dist/unitary.min.js')
-            .pipe(rename('unitary.browser.min.js'))
-            .pipe(gulp.dest('./dist'));
-
-        gulp.src('./dist/unitary.min.js.gz')
-            .pipe(rename('unitary.browser.min.js.gz'))
-            .pipe(gulp.dest('./dist'));
+        }
     });
 });
 gulp.task('default', ['minify']);
