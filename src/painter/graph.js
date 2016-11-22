9fc443047fc7a66ba88a23e9a026b350c5d813fb
幾野直人 <moon_in_france@yahoo.co.jp>
2016-11-22 14:31:33 +0900

    implement graph painter.

--- src/painter/graph.js
+++ src/painter/graph.js
@@ -5,13 +5,19 @@ export default function(obj) {
     if (end === null) end = this.canvasWidth - this.origin.x;
 
     const points = [];
-    for (let i = start; i <= end; i = 0|i+1) points[points.length] = new Unitary.Point(i, obj.f(i / obj.scale) * obj.scale);
+    for (let x = start; x <= end; x = 0|x+1) {
+        const slope = new Unitary.Vector(x, obj.f(x / obj.scale) * obj.scale)
+            .subtract(new Unitary.Vector(x + 1, obj.f((x + 1) / obj.scale) * obj.scale)).theta / (2 * Math.PI) * 360;
+
+        if (!Number.isNaN(obj.f(x / obj.scale))) points[points.length] = new Unitary.Point(x, obj.f(x / obj.scale) * obj.scale);
+        if (45 <= slope && slope <= 135 || 225 <= slope && slope <= 315) {
+            if (!Number.isNaN(obj.f((x + 0.5) / obj.scale))) points[points.length] = new Unitary.Point(x + 0.5, obj.f((x + 0.5) / obj.scale) * obj.scale);
+        }
+    }
 
     this.canvas.moveTo(this.X(points[0].x), this.Y(points[0].y));
     for (let i = 0, _i = points.length; i < _i; i = 0|i+1) {
         this.canvas.lineTo(this.X(points[i].x), this.Y(points[i].y));
-        this.canvas.moveTo(this.X(points[i].x), this.Y(points[i].y));
     }
-    this.canvas.closePath();
     this.canvas.stroke();
 };
