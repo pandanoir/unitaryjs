5952158f6c7692e9091ef63046b4e89494b75f2a
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2017-03-06 15:06:26 +0900

    set default scale of Graph to 30

--- src/classes/graph.js
+++ src/classes/graph.js
@@ -1,7 +1,7 @@
 import ContouredObject from './contouredobject.js';
 
 export default class Graph extends ContouredObject {
-    constructor(f, scale) {
+    constructor(f, scale = 30) {
         super();
         this.f = f;
         this.scale = scale;
