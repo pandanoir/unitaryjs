026eabdc007533c5d3a0ca409680f072a64a7368
pandanoir <moon_in_france@yahoo.co.jp>
2018-02-01 11:49:10 +0900

    install google-closure-compiler and remove gulp-closure-compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,26 @@
 const gulp = require('gulp');
-const closureCompiler = require('gulp-closure-compiler');
+const compilerPackage = require('google-closure-compiler');
+const closureCompiler = compilerPackage.gulp();
 const rename = require("gulp-rename");
 const gzip = require('gulp-gzip');
 
 gulp.task('minify', () => {
-    const flags = {
-            compilation_level: 'SIMPLE_OPTIMIZATIONS',
-            language_in: 'ECMASCRIPT6_STRICT',
-            language_out: 'ECMASCRIPT5_STRICT',
-            warning_level: 'QUIET'
-        };
-    gulp.src('./dist/canvas.js')
-    .pipe(closureCompiler({
-        fileName: 'canvas.min.js',
-        compilerFlags: flags
-    }))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(gzip()).pipe(gulp.dest('./dist/'));
+    closureCompiler({
+        js: './dist/canvas.js',
+        compilation_level: 'SIMPLE',
+        language_in: 'ECMASCRIPT6_STRICT',
+        language_out: 'ECMASCRIPT5_STRICT',
+        js_output_file: 'canvas.min.js'
+    }).src()
+    .pipe(gulp.dest('./dist/'));
 
-    gulp.src('./dist/unitary.js')
-    .pipe(closureCompiler({
-        fileName: 'unitary.min.js',
-        compilerFlags: flags
-    }))
+    closureCompiler({
+        js: './dist/unitary.js',
+        compilation_level: 'SIMPLE',
+        language_in: 'ECMASCRIPT6_STRICT',
+        language_out: 'ECMASCRIPT5_STRICT',
+        js_output_file: 'unitary.min.js'
+    }).src()
     .pipe(gulp.dest('./dist/'))
     .pipe(gzip()).pipe(gulp.dest('./dist/'))
     .on('end', () => {
