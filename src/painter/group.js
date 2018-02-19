ca873256631b81420078f34e672c7bce898c6621
pandanoir <moon_in_france@yahoo.co.jp>
2018-02-19 22:28:08 +0900

    fix group painter

--- src/painter/group.js
+++ src/painter/group.js
@@ -8,38 +8,39 @@ export default function(obj) {
     for (let i = 0, _i = lineProps.length; i < _i; i++) {
         const p = lineProps[i];
         if (obj[`getLine${p}`] && obj[`getLine${p}`]() !== null)
-            groupStyle[p] = obj[p]();
+            groupStyle[p] = obj[`getLine${p}`]();
+        else groupStyle[p] = null;
     }
     for (let i = 0, _i = obj.group.length; i < _i; i = 0|i+1) {
         // this method sets strokeStyle and fillStyle.
-        let _obj = obj.group[i];
+        const _obj = obj.group[i];
         const NULL = [];
-        let before = {
+        const before = {
             fillStyle: NULL,
             strokeStyle: NULL,
         };
 
-        if (!_obj.style || _obj.style.fillStyle === null) {
+        if (_obj.style && _obj.style.fillStyle === null) {
             before.fillStyle = _obj.style.fillStyle;
             _obj.style.fillStyle = groupStyle.fillStyle;
         }
-        if (!_obj.style || _obj.style.strokeStyle === null) {
+        if (_obj.style && _obj.style.strokeStyle === null) {
             before.strokeStyle = _obj.style.strokeStyle;
             _obj.style.strokeStyle = groupStyle.strokeStyle;
         }
-        // for (let i = 0, _i = lineProps.length; i < _i; i++) {
-        //     const p = lineProps[i];
-        //     if ((!_obj[`getLine${p}`] || _obj[`getLine${p}`]() === null) && groupStyle[p] != null) {
-        //         before[p] = _obj[`getLine${p}`]();
-        //         _obj[`setLine${p}`](groupStyle[p]);
-        //     } else before[p] = NULL;
-        // }
-        //
+        for (let j = 0, _j = lineProps.length; j < _j; j++) {
+            const p = lineProps[j];
+            if ((!_obj[`getLine${p}`] || _obj[`getLine${p}`]() === null) && groupStyle[p] != null) {
+                before[p] = _obj[`getLine${p}`]();
+                _obj[`setLine${p}`](groupStyle[p]);
+            } else before[p] = NULL;
+        }
+
         this.__drawHelper__(_obj);
-        if (before.fillStyle !== NULL) _obj.style.fillStyle = before.fillStyle;
-        if (before.strokeStyle !== NULL) _obj.style.fillStyle = before.fillStyle;
-        for (let i = 0, _i = lineProps.length; i < _i; i++) {
-            const p = lineProps[i];
+        if (_obj.style && before.fillStyle !== NULL) _obj.style.fillStyle = before.fillStyle;
+        if (_obj.style && before.strokeStyle !== NULL) _obj.style.fillStyle = before.fillStyle;
+        for (let j = 0, _j = lineProps.length; j < _j; j++) {
+            const p = lineProps[j];
             if (before[p] !== NULL) {
                 _obj[`setLine${p}`](before[p]);
             }
